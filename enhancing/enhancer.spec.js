2b94e25d1ee185e2524697b65e5142633123ca65
Lambda_School_Loaner_85 <lambda_school_loaner_85@Lambda-School-Loaner-85s-MacBook-Pro.local>
2019-05-13 13:12:05 -0500

    repair tested and working

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,9 @@
-const enhancer = require('./enhancer.js');
+const { succeed, repair, fail, get } = require('./enhancer.js');
 // test away!
+
+
+it('takes in an item and changes durability to', () =>{
+    expect (repair('fish').durability).toBe(100);
+    expect (repair(NaN).durability).toBe(100);
+    expect (repair(999).durability).toBe(100);
+})
